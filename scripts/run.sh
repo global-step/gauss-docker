68711889b1474f754891b93292321812b7187a07
Shohei Kameda <shoheik@cpan.org>
2019-01-16 11:29:27 +0900

    added webpacker dev

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,8 +7,8 @@ gem uninstall did_you_mean
 bundle install --path vendor/bundle
 yarn install
 bundle exec rake db:migrate
-bundle exec rake jobs:work & 
+bundle exec rake jobs:work &
+bundle exec bin/webpack-dev-server &
 bundle exec rails s &
-
 
 /usr/share/elasticsearch/bin/elasticsearch -Des.insecure.allow.root=true 
